ad8c15b8236dee0302b2318b7228f1b42fcca86c
supernatv <ve.supanat_st@tni.ac.th>
2015-11-24 16:31:40 +0700

    Test add space

--- next_bus/tuple.py
+++ next_bus/tuple.py
@@ -4,5 +4,6 @@ print( now )
 months = ('-error-', 'January','February','March','April','May','June', 'July','August','September','October','November','  December')
 print( months[now.month] )
 
+
 for x in months:
     print x
