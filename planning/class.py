07455c200c1e54a40dabdba4e204428b22b06cbb
supernatv <ve.supanat_st@tni.ac.th>
2015-12-01 17:10:30 +0700

    add other classes

--- planning/class.py
+++ planning/class.py
@@ -20,3 +20,43 @@ class Users(object):
         return self.passport_id
     def getPicId(self):
         return self.pic_id
+
+class Place(object):
+    
+    def __init__(self,name,location,price,review):#nut
+        self.nameofplace= nameofplace
+        self.location   = location
+        self.price      = price
+        self.review     = review
+        
+class Accommodation(object):#korn
+    
+    def __init__(self,nameofacc,location,promotion,review):
+        self.nameofacc      = nameofacc
+        self.location       = location
+        self.promotion      = promotion
+        self.review         = review
+        
+class Gallery(object):#tops
+    def __init__(self,picture,message,video):
+        self.picture = picture
+        self.message = message
+        self.video   = video
+
+class Promotion(object):#Petch
+    def __init__(self,price,location,condition,accommodation):
+        self.price           = price
+        self.location        = location
+        self.condition       = condition
+        self.accommodation   = accommodation
+
+class Trip(object):#Tuk
+    def __init__(self,user, gallory, accommodation, place, promotion, plan, tag, chat):
+        self.user           = user
+        self.gallory        = gallory
+        self.accommodation  = accommodation
+        self.place          = place
+        self.promotion      = promotion
+        self.plan           = plan
+        self.tag            = tag
+        self.chat           = chat
